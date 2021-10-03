5baa95b5dc3c241585d4e79df5ba18cc4bef5dfc
Hamzat Ajanaku <hamzatajanaku@gmail.com>
2021-10-03 11:12:52 +0100

    Refactored the code a bit

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const SearchBox = ({ searchChange }) => {
+  function preventSubmit (e) {
+    e.preventDefault()
+  }
   return (
-    <form className="mb-5 mx-auto" style={{ maxWidth: 200 }}>
+    <form className="mb-5 mx-auto" style={{ maxWidth: 200 }} onSubmit={preventSubmit}>
       <input type='Search' placeholder='Search Robots' className='form-control me-3' onChange={searchChange} />
     </form>
   )
--- src/containers/App.js
+++ src/containers/App.js
@@ -31,6 +31,7 @@ function App () {
   )
 
   const onSearchChange = (e) => {
+    e.preventDefault()
     setSearchField( e.target.value )
   }
 
